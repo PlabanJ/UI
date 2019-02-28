d0bd97d1c5161339331228ed838bfdfae1040af6
Anurag Garg <anurag@geekyants.com>
2019-02-28 12:30:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 console.log('Hey! there');
 
-console.log("Hy its new---!! Bye Bye Srishh");
 
-console.log("Hello Vikram");
 
-console.log('Print: I am coming from release...!');	
-
-console.log('Print: I am coming from release too...!');
+console.log('Print: I am Anurag');
